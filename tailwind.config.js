de6a55aa10b72dfd19f5ee90d3002478359734ef
bl00p1ng <blooping@protonmail.com>
2024-08-18 22:29:48 -0500

    Configuración del tema de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#16161a',
+        headline: '#fffffe',
+        paragraph: '#94a1b2',
+        button: '#7f5af0',
+        'button-text': '#fffffe',
+        stroke: '#010101',
+        main: '#fffffe',
+        highlight: '#7f5af0',
+        secondary: '#72757e',
+        tertiary: '#2cb67d',
+      },
+    },
   },
   plugins: [],
 }
